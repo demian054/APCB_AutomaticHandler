20ce68b76904f1bc7f121b5b7e7a90c6929c50bb
dbolivar <demian054m@gmail.com>
2015-11-16 21:57:52 -0430

    Agregando librerias y entidades

---
+++ src/com/apcb/automaticHandler/theats/AutomaticProcesor.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.apcb.automaticHandler.theats;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+/**
+ *
+ * @author Demian
+ */
+public class AutomaticProcesor extends Thread{
+    int timeSleep;
+    int repeat;
+    boolean finish;
+    
+    public static void main(String[] args) throws Exception {
+        AutomaticProcesor auto;
+        
+        HashMap<String,String> configArgs = new HashMap<>();
+        
+        configArgs.put("timeSleep", "1");
+        configArgs.put("repeat", "0");
+        configArgs.put("name", "AutomaticProcesor");
+        
+        for (String arg : args) {
+            try {
+                String[] keyValue = arg.split("=");
+                if (configArgs.get(keyValue[0])!=null){
+                    configArgs.put(keyValue[0], keyValue[1]); 
+                } else {
+                    log.debug("argumento no reconocido = "+arg);
+                }   
+            } catch (Exception e) {
+                log.debug("argumento incorrecto = "+arg);
+            }    
+        }
+        
+        auto = new AutomaticProcesor(configArgs.get("timeSleep"),configArgs.get("repeat"),configArgs.get("name")); 
+        auto.start();
+    
+    }
+    
+
+    public AutomaticProcesor(String timeSleep, String repeat, String name) {
+        this.timeSleep = new Integer(timeSleep)*1000;
+        this.repeat = new Integer(repeat);
+        this.setName(name);
+        finish = this.repeat>0;  
+    }
+
+    @Override
+    public void run(){
+        log.debug("Iniciando "+this.getName());
+        while (repeat > 0 || !finish){
+        //for(int i=0;i<repeat;i++){
+            log.debug("<"+new Date()+"> "+this.getName()+" : "+(repeat*-1));
+            
+            
+            try {
+                AutomaticProcesor.sleep(timeSleep);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(AutomaticProcesor.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+            repeat--;
+            
+        }
+    }
+    
+    
+}
